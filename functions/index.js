56b556e9b400e62479b5aae68dc07e2e47ab3295
Tom Juszczyk <tomkj4@gmail.com>
2018-11-06 22:59:41 -0600

    voters

--- functions/index.js
+++ functions/index.js
@@ -2,21 +2,28 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp();
 
-const statsRef = admin.firestore().doc('stats/votes');
-
 exports.countVotes = functions.firestore.document('votes/{name}')
   .onWrite((change, context) => {
+    const statsRef = admin.firestore().doc('stats/votes');
     return admin.firestore().runTransaction(t => {
       return t.get(statsRef).then(votes => {
-        const oldChoice = change.before.data() ? change.before.data().color : null;
-        const newChoice = change.after.data().color;
-        const newCount = votes.data() || {};
-        const oldColorCount = newCount[newChoice] || 0;
-        newCount[newChoice] = oldColorCount + 1;
-        if (oldChoice) {
-            newCount[oldChoice] = newCount[oldChoice] ? newCount[oldChoice] - 1 : 0;
+        const totals = votes.data() || {};
+
+        // decrement previous choice
+        if (change.before.data()) {
+          const oldChoice = change.before.data().color;
+          totals[oldChoice] = totals[oldChoice] ? totals[oldChoice] - 1 : 0;
         }
-        t.set(statsRef, newCount);
+
+        // increment current choice
+        if (change.after.data()) {
+          const newChoice = change.after.data().color;
+          totals[newChoice] = totals[newChoice] ? totals[newChoice] + 1 : 1;
+        }
+
+        // save new totals
+        t.set(statsRef, totals);
       })
     })
+
   })
